69e623be14715d4f07883a57c933aa86aeac8f57
zhangzhenli <zhangzhenli@live.com>
2018-07-03 15:46:25 +0800

    Add extra processing to the accessToken, which is usually not obtained by Authorization Code Grant Flow，For example, it may obtained by Implicit Grant Flow

--- pac4j-oauth/src/main/java/org/pac4j/oauth/credentials/OAuth20Credentials.java
+++ pac4j-oauth/src/main/java/org/pac4j/oauth/credentials/OAuth20Credentials.java
@@ -1,7 +1,8 @@
 package org.pac4j.oauth.credentials;
 
-import com.github.scribejava.core.model.OAuth2AccessToken;
 import org.pac4j.core.util.CommonHelper;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
 
 /**
  * This class represents an OAuth credentials for OAuth 2.0 an authorization code.
@@ -18,30 +19,24 @@ public class OAuth20Credentials extends OAuthCredentials {
     /**
      * For OAuth2 Authorization Code Flow.
      *
-     * @param code       the authorization code
+     * @param code the authorization code
      */
     public OAuth20Credentials(String code) {
         this.code = code;
     }
 
+    /**
+     * For OAuth2 Other Flow.
+     *
+     * @param accessToken the accessToken.
+     */
+    public OAuth20Credentials(OAuth2AccessToken accessToken) {
+        this.code = null;
+        this.accessToken = accessToken;
+    }
+
     public String getCode() {
         return code;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        OAuth20Credentials that = (OAuth20Credentials) o;
-
-        return code != null ? code.equals(that.code) : that.code == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return code != null ? code.hashCode() : 0;
     }
 
     public OAuth2AccessToken getAccessToken() {
@@ -53,9 +48,27 @@ public class OAuth20Credentials extends OAuthCredentials {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OAuth20Credentials that = (OAuth20Credentials) o;
+
+        if (code != null ? !code.equals(that.code) : that.code != null) return false;
+        return accessToken != null ? accessToken.equals(that.accessToken) : that.accessToken == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = code != null ? code.hashCode() : 0;
+        result = 31 * result + (accessToken != null ? accessToken.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         return CommonHelper.toNiceString(this.getClass(),
-                "code", code,
-                "accessToken", accessToken);
+            "code", code,
+            "accessToken", accessToken);
     }
 }
